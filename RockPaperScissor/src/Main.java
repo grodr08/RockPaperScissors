4542234147eaf07185508a0df8150516a1c0c24a
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-09-29 04:23:22 +0100

    printIntroToGame function created it ask user for name and lays the point system of the game

--- RockPaperScissor/src/Main.java
+++ RockPaperScissor/src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello User Welcome to my Game!");
+
+    printIntroToGame();
+
+    }
+
+    public static void printIntroToGame() {
+        Scanner scanner = new Scanner(System.in);
+        String userName;
+        System.out.println("What is your name:");
+        userName = scanner.next();
+
+        System.out.println("Welcome " + userName + "!!!");
+        System.out.println("This is a Rock Paper Scissor game.");
+        System.out.println("Your objective is to beat the computer.");
+        System.out.println("The first one to get three points wins.");
     }
 }

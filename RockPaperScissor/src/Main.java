4b5a4f93043e4c75100627e94f9d809f93a00f1d
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-09-29 04:32:03 +0100

    arrayOfAnswers function returns Rock, Paper or Scissor

--- RockPaperScissor/src/Main.java
+++ RockPaperScissor/src/Main.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
     printIntroToGame();
+    System.out.println(arrayOfAnswers());
 
     }
 
@@ -17,5 +19,18 @@ public class Main {
         System.out.println("This is a Rock Paper Scissor game.");
         System.out.println("Your objective is to beat the computer.");
         System.out.println("The first one to get three points wins.");
+    }
+
+    public static String arrayOfAnswers() {
+        // Array containing the 3 answers
+        String[] rockPaperScissorArray = {"Rock", "Paper", "Scissor"};
+
+        Random random = new Random();
+
+        // Generates a random index within the bounds fo the array
+        int randomIndex = random.nextInt(rockPaperScissorArray.length);
+
+        // Get the random string from the array
+        return rockPaperScissorArray[randomIndex];
     }
 }

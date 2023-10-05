76d71a1f134f1f6db74041de690c51c2bc0b1ef3
Giancarlos Rodriguez <91751980+grodr08@users.noreply.github.com>
2023-10-05 02:03:09 +0100

    Main game is done

--- RockPaperScissor/src/Main.java
+++ RockPaperScissor/src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,7 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
     printIntroToGame();
-    askForUserInput();
     compareValues();
 
     }
@@ -18,12 +16,13 @@ public class Main {
         userName = scanner.next();
 
         // Intro to the Game & Explains the points system
-        System.out.println("||----------------------------------------||");
-        System.out.println("|| Welcome " + userName + "!!!                         ||");
-        System.out.println("|| This is a Rock Paper Scissor game.     ||");
-        System.out.println("|| Your objective is to beat the computer.||");
-        System.out.println("|| The first one to get three points wins.||");
-        System.out.println("||----------------------------------------||");
+        String line = "----------------------------------------";
+        System.out.println(line);
+        System.out.println("Welcome " + userName + "!!!");
+        System.out.println("This is a Rock Paper Scissor game.");
+        System.out.println("Your objective is to beat the computer.");
+        System.out.println("The first one to get three points wins.");
+        System.out.println(line);
     }
 
     public static String arrayOfAnswers() {
@@ -51,24 +50,53 @@ public class Main {
         do {
             System.out.println("You must choose between 'Rock', 'Paper' or 'Scissor':");
             userAnswer = scanner.next().toLowerCase();
-        }
-        while (userAnswer.equals("rock") || userAnswer.equals("paper") || userAnswer.equals("scissor"));
 
-        return userAnswer = scanner.next();
+        }
+        while (!userAnswer.equals("rock") && (!userAnswer.equals("paper")) && (!userAnswer.equals("scissor")));
+
+        return userAnswer;
     }
 
     public static void compareValues() {
-        String computerAnswer = arrayOfAnswers();
-        String userAnswer = askForUserInput();
 
         int computerPoints = 0;
         int userPoints = 0;
 
-//        while (computerPoints < 3 || userPoints < 3) {
-//
-//        }
+        while (computerPoints < 3 && userPoints < 3) {
+            String computerAnswer = arrayOfAnswers();
+            String userAnswer = askForUserInput();
+            System.out.println(computerAnswer);
 
-        System.out.println(computerAnswer);
-        System.out.println(userAnswer);
+            if (computerAnswer.equals("rock") && userAnswer.equals("paper")) {
+                userPoints++;
+            }
+            else if (computerAnswer.equals("rock") && userAnswer.equals("scissor")) {
+                computerPoints++;
+            }
+            else if (computerAnswer.equals("rock") && userAnswer.equals("rock")) {
+                System.out.println("Is a tied");
+            }
+            else if (computerAnswer.equals("paper") && userAnswer.equals("scissor")) {
+                userPoints++;
+            }
+            else if (computerAnswer.equals("paper") && userAnswer.equals("rock")) {
+                computerPoints++;
+            }
+            else if (computerAnswer.equals("paper") && userAnswer.equals("paper")) {
+                System.out.println("Is a tied");
+            }
+            else if (computerAnswer.equals("scissor") && userAnswer.equals("rock")) {
+                userPoints++;
+            }
+            else if (computerAnswer.equals("scissor") && userAnswer.equals("paper")) {
+                computerPoints++;
+            }
+            else {
+                System.out.println("Is a tied");
+            }
+
+            System.out.println("User Points: " + userPoints);
+            System.out.println("Computer Points: " + computerPoints);
+        }
     }
 }
